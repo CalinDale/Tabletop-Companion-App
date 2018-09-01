eebcc40e0d490a65edd2fe4e5135d5d3f7a5b934
Calin Dale <citdale@gmail.com>
2018-09-02 11:58:54 +1200

    Fixed Character Page component test

--- src/app/character-page/character-page.component.spec.ts
+++ src/app/character-page/character-page.component.spec.ts
@@ -1,3 +1,6 @@
+import { MessageService } from './../message.service';
+import { Character} from '../character';
+import { CharacterService } from './../character.service';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { CharacterDetailsComponent } from './../character-details/character-details.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
@@ -5,33 +8,37 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CharacterPageComponent } from './character-page.component';
 
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabaseModule, AngularFireList } from 'angularfire2/database';
 
 import { environment } from '../../environments/environment';
 
 describe('CharacterPageComponent', () => {
+  let testCharactersList: AngularFireList<Character>;
+  let testCharacterService: CharacterService;
+  let testMesageService: MessageService;
   let component: CharacterPageComponent;
-  let fixture: ComponentFixture<CharacterPageComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        CharacterPageComponent,
-        CharacterDetailsComponent
-      ],
-      imports: [
-        AngularFireAuthModule,
-        AngularFireModule.initializeApp(environment.firebase),
-        AngularFireDatabaseModule // for database
-      ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(CharacterPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testCharactersList = jasmine.createSpyObj('testCharactersList', [
+      'snapshotChanges'
+    ]);
+    (<jasmine.Spy>(testCharactersList.snapshotChanges)).and.returnValue( { pipe() {} } );
+    testCharacterService = jasmine.createSpyObj('testCharacterService', [
+      'getCharactersList',
+      'deleteAll',
+    ]);
+    (<jasmine.Spy>(testCharacterService.getCharactersList)).and.returnValue(testCharactersList);
+
+    testMesageService = jasmine.createSpyObj('testMessageService', [
+      'add'
+    ]);
+
+    component = new CharacterPageComponent(testCharacterService, testMesageService);
+  });
+  afterEach(() => {
+    testCharactersList = null;
+    testCharacterService = null;
+    testMesageService = null;
+    component = null;
   });
 
   // TODO: See character.service.spec for info on cannot read property of null
