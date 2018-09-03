1ff64237833f33f6d3f3cbb233d6ae0dfbf718f8
Calin Dale <citdale@gmail.com>
2018-09-03 15:32:00 +1200

    Tests Fixed, Still investigating. Look at test changes to see what functions need changes.

--- src/app/attribute.service.ts
+++ src/app/attribute.service.ts
@@ -1,7 +1,7 @@
 import { Attribute } from './attribute';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
-import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireList, AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +12,10 @@ export class AttributeService {
 
   userID: string;
   characterID: string;
+  attributeID: string;
 
   attributesRef: AngularFireList<Attribute> = null;
+  attributeRef: AngularFireObject<Attribute> = null;
 
   constructor(private db: AngularFireDatabase, private afAuth: AngularFireAuth) {
     this.afAuth.authState.subscribe(user => {
@@ -31,21 +33,38 @@ export class AttributeService {
     return this.characterID;
   }
 
+  setAttributeID(key: string) {
+    if (key != null) {
+      this.attributeID = key;
+    }
+  }
+
+  getAttributeID() {
+    return this.attributeID;
+  }
+
+
+  getAttributes(key: string): AngularFireList<Attribute> {
+    // tslint:disable-next-line:curly
+    if (!this.userID) return;
+    this.attributesRef = this.db.list(`attributes/${this.userID}/${key}/`);
+    return this.attributesRef;
+  }
+
   createAttribute(attribute: Attribute): void {
-    this.attributesRef = this.db.list(`attributes/${this.userID}`);
+    this.attributesRef = this.db.list(`attributes/${this.userID}/${this.characterID}`);
     this.attributesRef.push(attribute);
   }
 
-  updateAttribute(name: string, value: any): void {
-    this.attributesRef.update(name, value).catch(error => this.handleError(error));
+  // TODO: this.db.object is not a function?
+  updateAttribute(attribute: Attribute): void {
+    this.attributeID = this.getAttributeID();
+    this.attributeRef = this.db.object(`attributes/${this.userID}/${this.characterID}/${this.attributeID}`);
+    this.attributeRef.update(attribute).catch(error => this.handleError(error));
   }
 
   deleteAttribute(name: string): void {
     this.attributesRef.remove(name).catch(error => this.handleError(error));
-  }
-
-  getAttributesList(): AngularFireList<Attribute> {
-    return this.attributesRef;
   }
 
   deleteAll(): void {
