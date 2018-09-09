ef4f99b5ae2dc002e02cb88c75e7724071fd209e
Calin Dale <citdale@gmail.com>
2018-09-09 15:45:26 +1200

    Test for character-list-entry.component set up

--- src/app/character-list-entry/character-list-entry.component.spec.ts
+++ src/app/character-list-entry/character-list-entry.component.spec.ts
@@ -1,48 +1,25 @@
 import { CharacterDetailsComponent } from '../character-details/character-details.component';
 import { MessageService } from '../message.service';
-import { CharacterService } from '../character.service';
-
 import { CharacterListEntryComponent } from './character-list-entry.component';
-
-import { AttributeService } from '../attribute.service';
-
-import { Router } from '@angular/router';
 import { Character } from '../character';
 
 describe('CharacterListEntryComponent', () => {
-  let testCharacterService: CharacterService;
   let testMessageService: MessageService;
-  let testAttributeSerice: AttributeService;
-  let testRouter: Router;
   let component: CharacterListEntryComponent;
   beforeEach(() => {
-    testCharacterService = jasmine.createSpyObj('testCharacterService', [
-      'deleteCharacter',
-      'updateCharacter'
-    ]);
     testMessageService = jasmine.createSpyObj('testMessageService', [
       'add'
     ]);
-    testAttributeSerice = jasmine.createSpyObj('testAttributeService', [
-      'setCharacterID'
-    ]);
-    testRouter = jasmine.createSpyObj('testRouter', [
-      'navigateByUrl'
-    ]);
+
     component = new CharacterListEntryComponent(
-      testCharacterService,
-      testMessageService,
-      testAttributeSerice,
-      testRouter
+      testMessageService
     );
   });
   afterEach(() => {
-    testCharacterService = null;
     testMessageService = null;
-    testAttributeSerice = null;
-    testRouter = null;
     component = null;
   });
+
   describe('with valid character', () => {
     let testCharacter: Character;
     beforeEach(() => {
@@ -52,6 +29,7 @@ describe('CharacterListEntryComponent', () => {
     afterEach(() => {
       testCharacter = null;
     });
+
     describe('with characterDetailsComponent', () => {
       let testCharacterDetails: CharacterDetailsComponent;
       beforeEach(() => {
@@ -62,10 +40,6 @@ describe('CharacterListEntryComponent', () => {
       });
       afterEach(() => {
         testCharacter = null;
-      });
-      it('editCharacter() should setCharacterID in attributeService with CharacterID', () => {
-        component.editCharacter();
-        expect(testAttributeSerice.setCharacterID).toHaveBeenCalledWith(testCharacter.key);
       });
       it('editCharacter() should setCharacter in characterDetails with Character', () => {
         component.editCharacter();
