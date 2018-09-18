c0d61d62716381ce2efe9b3e255844f8892f8758
Calin Dale <citdale@gmail.com>
2018-09-19 10:35:42 +1200

    Attempted refactoring

--- src/app/tracker-row/tracker-row.component.ts
+++ src/app/tracker-row/tracker-row.component.ts
@@ -1,3 +1,4 @@
+import { TrackerService } from './../tracker.service';
 import { AttributeService } from './../attribute.service';
 import { Character } from './../character';
 import { Attribute } from './../attribute';
@@ -15,6 +16,10 @@ export class TrackerRowComponent implements OnInit {
   @Input()attributeColumns: Attribute[];
   @Input()currentActor: number;
   @Input()index: number;
+
+  // TODO: get characters from service instead of tracker component.
+  // Need to master having observables continually stream new updates for this.
+  // then can have order changing functions update tracker service which will cause tracker to update.
   @Input()characters: Character[];
 
   attributes: Attribute[];
@@ -22,11 +27,14 @@ export class TrackerRowComponent implements OnInit {
   selectedAttributeIndexes: number[] = [];
 
   constructor(
-    private attributeService: AttributeService
+    private attributeService: AttributeService,
+    private trackerService: TrackerService
   ) { }
 
   ngOnInit() {
     this.retrieveAttributes();
+    // See Above
+    // this.retrieveCharacters();
   }
 
   onChange(i: number) {
@@ -38,12 +46,14 @@ export class TrackerRowComponent implements OnInit {
   }
 
   retrieveAttributes() {
-    this.attributeService.getAttributes(this.character.key).snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-      )
-    ).subscribe(attributes => {
+    this.trackerService.getAttributes(this.character.key).subscribe(attributes => {
       this.attributes = attributes;
+    });
+  }
+
+  retrieveCharacters() {
+    this.trackerService.getCharacters().subscribe(characters => {
+      this.characters = characters;
     });
   }
 
