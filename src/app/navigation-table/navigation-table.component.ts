0dbbe4eba9a390ad873abf0fb062747ada8098e7
Calin Dale <citdale@gmail.com>
2018-09-23 17:17:12 +1200

    Now highlights the page on load

--- src/app/navigation-table/navigation-table.component.ts
+++ src/app/navigation-table/navigation-table.component.ts
@@ -1,15 +1,21 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '../../../node_modules/@angular/common';
 
 @Component({
   selector: 'app-navigation-table',
   templateUrl: './navigation-table.component.html',
-  styleUrls: ['./navigation-table.component.css']
+  styleUrls: ['./navigation-table.component.css'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 export class NavigationTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) {
+
+  }
 
   ngOnInit() {
+    this.selectOnOpen();
   }
 
   select(event) {
@@ -18,6 +24,16 @@ export class NavigationTableComponent implements OnInit {
       elements[i].classList.remove('selected'); // clear old active cells
     }
     event.currentTarget.classList.add('selected');
+  }
+
+  selectOnOpen() {
+    if (this.location.path().includes('app')) {
+      document.getElementById('appLink').classList.add('selected');
+    } else if (this.location.path().includes('login')) {
+      document.getElementById('accountLink').classList.add('selected');
+    } else {
+      document.getElementById('homeLink').classList.add('selected');
+    }
   }
 
 }
