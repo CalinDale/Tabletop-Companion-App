745dab27b62aae9802fd730727e2b4e974721c38
Calin Dale <citdale@gmail.com>
2018-09-23 16:04:01 +1200

    Made nav bar highlight current page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tabletop-Companion-App';
+
+  select(event) {
+    const elements = document.getElementsByClassName('selected');
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.remove('selected'); // clear old active cells
+    }
+    event.currentTarget.classList.add('selected');
+  }
 }
