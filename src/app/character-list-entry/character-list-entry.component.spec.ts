7eec8a3facf837d4654de2d4a653a7eac1d2a787
Calin Dale <citdale@gmail.com>
2018-09-26 09:28:58 +1200

    Prepared for tests

--- src/app/character-list-entry/character-list-entry.component.spec.ts
+++ src/app/character-list-entry/character-list-entry.component.spec.ts
@@ -1,3 +1,5 @@
+import { of, Observable } from 'rxjs';
+import { AngularFireList } from 'angularfire2/database';
 import { TrackerService } from './../tracker.service';
 import { AttributeService } from './../attribute.service';
 import { CharacterService } from './../character.service';
@@ -5,12 +7,17 @@ import { CharacterDetailsComponent } from '../character-details/character-detail
 import { MessageService } from '../message.service';
 import { CharacterListEntryComponent } from './character-list-entry.component';
 import { Character } from '../character';
+import { Attribute } from '../attribute';
 
 describe('CharacterListEntryComponent', () => {
   let testCharacter: Character;
   let testCharacterDetails: CharacterDetailsComponent;
   let testMessageService: MessageService;
   let testTrackerService: TrackerService;
+  let testCharacterService: CharacterService;
+  let testAttributes: Attribute[];
+  let testAngularFireList: AngularFireList<Attribute>;
+  let testAttributeService: AttributeService;
   let component: CharacterListEntryComponent;
   beforeEach(() => {
     testCharacter = <Character>{ key: 'Grog23'};
@@ -25,16 +32,39 @@ describe('CharacterListEntryComponent', () => {
       'addToTracker',
       'removeFromTracker'
     ]);
+    testCharacterService = jasmine.createSpyObj('testCharacterService', [
+      'createCharacter',
+      'getCharacterID'
+    ]);
+    (<jasmine.Spy>testCharacterService.getCharacterID).and.returnValue(testCharacter.key);
+
+    testAttributes = <any>[ { key: 'attribute0' } ];
+    testAngularFireList = <any>{
+      snapshotChanges(): Observable<Attribute[]> { return (of(testAttributes)); }
+    };
+
+    testAttributeService = jasmine.createSpyObj('testAttributeService', [
+      'getAttributes',
+      'cloneAttributes'
+    ]);
+    (<jasmine.Spy>testAttributeService.getAttributes).and.returnValue(testAngularFireList);
 
     component = new CharacterListEntryComponent(
       testMessageService,
-      testTrackerService
+      testTrackerService,
+      testCharacterService,
+      testAttributeService
     );
   });
   afterEach(() => {
+    testCharacter = null;
     testCharacterDetails = null;
     testMessageService = null;
     testTrackerService = null;
+    testCharacterService = null;
+    testAttributes = null;
+    testAngularFireList = null;
+    testAttributeService = null;
     component = null;
   });
 
