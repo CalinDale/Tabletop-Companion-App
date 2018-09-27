7b38fd49074fbe5146d0b2b364392469a13e41da
Calin Dale <citdale@gmail.com>
2018-09-27 16:26:17 +1200

    more style and some test fix

--- src/app/user-profile/user-profile.component.spec.ts
+++ src/app/user-profile/user-profile.component.spec.ts
@@ -1,11 +1,13 @@
 import { AuthService } from '../core/auth.service';
 import { UserProfileComponent } from './user-profile.component';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '../../../node_modules/@angular/router';
 
 describe('UserProfileComponent', () => {
   let testAuth: AuthService;
   let testFB: FormBuilder;
   let component: UserProfileComponent;
+  let testRouter: Router;
   beforeEach(() => {
     testAuth = jasmine.createSpyObj('testAuth', [
       'needsomethinghere'
@@ -13,7 +15,10 @@ describe('UserProfileComponent', () => {
     testFB = jasmine.createSpyObj('testFB', [
       'needsomethinghere'
     ]);
-    component = new UserProfileComponent(testAuth, testFB);
+    testRouter = jasmine.createSpyObj('testRouter', [
+      'navigate'
+    ]);
+    component = new UserProfileComponent(testAuth, testFB, testRouter);
   });
   afterEach(() => {
     testAuth = null;
